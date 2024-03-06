e843504454ed22b7d5d9fa40a2e511b35f0337fa
Prakash <pjhakumar519@gmail.com>
2024-03-06 16:40:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".text", {
+  strings: ["FrontEnd Developer", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+function popup() {
+  alert("hello");
+}
